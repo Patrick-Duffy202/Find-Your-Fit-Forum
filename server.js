048aa437513125f887edc4f4ac6f5e87893b2ef9
db <jacboysent@gmail.com>
2022-11-17 20:05:12 -0600

    break

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const helpers = require('./utils/helpers');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
