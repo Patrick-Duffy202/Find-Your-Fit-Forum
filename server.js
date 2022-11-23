792207745e419264a0c4b15e5b9dda050d9f4fb0
Darby Biddy <darbybiddy3@gmail.com>
2022-11-22 19:51:03 -0600

    merge conflicts

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ const sess = {
   })
 };
 
+app.use(session(sess));
+app.use(upload, [multer]);
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session(sess));
@@ -32,8 +36,7 @@ app.use(upload, [multer]);
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get("/", express.static(path.join(__dirname, "./uploads")))
+app.get("/", express.static(path.join(__dirname, "./uploads")))
 
 // turn on routes
 app.use(routes);
