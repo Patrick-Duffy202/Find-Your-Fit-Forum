e1a9b24143cfad90d80683b5f84fe2d69b3a6545
Darby Biddy <darbybiddy3@gmail.com>
2022-12-05 08:50:02 -0600

    fixing heroku push err

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 // import all models
-const Post = require('./Post');
-const User = require('./User');
-const Vote = require('./Vote');
-const Comment = require('./Comment');
+const Post = require('./Publish');
+const User = require('./Person');
+const Vote = require('./Castvote');
+const Comment = require('./Opinion');
 
 // create associations
 User.hasMany(Post, {
